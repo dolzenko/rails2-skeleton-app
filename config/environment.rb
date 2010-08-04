660827197fe54b3f54dc1c9315a9b788ab2b9e0a
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-08-04 17:39:37 +0400

    HAML/SASS

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "rspec", :lib => false, :version => "1.3.0"
   config.gem "rspec-rails", :lib => false, :version => "1.3.2"
   config.gem "shoulda", :lib => false, :version => "2.11.3"
+  config.gem "haml", :version => "3.0.15"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
